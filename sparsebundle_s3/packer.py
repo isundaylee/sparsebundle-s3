14d44bcb60d375c554e1e125946fd2eb91eeb344
Jiahao Li <reg@ljh.me>
2019-04-28 13:08:51 -0400

    Class-ifies packer

--- sparsebundle_s3/packer.py
+++ sparsebundle_s3/packer.py
@@ -9,101 +9,109 @@ import time
 
 from pathlib import Path
 
-logger = logging.getLogger('packer')
 
 BACK_PRESSURE_LIMIT = 3
 BACK_PRESSURE_SLEEP_INTERVAL = 1
 
 
-def pack(bundle, bundle_files, outdir, package_count, package_queue,
-         stop_event):
-    # Read band size
-    plist_path = os.path.join(bundle, 'Info.plist')
-    if not os.path.exists(plist_path):
-        logger.critical('Info.plist does not exist: %s', plist_path)
-        exit(1)
+class Packer:
+    def __init__(self, bundle, bundle_files, outdir, package_count, package_queue, stop_event):
+        self.bundle = bundle
+        self.bundle_files = bundle_files
+        self.outdir = outdir
+        self.package_count = package_count
 
-    with open(plist_path, 'rb') as f:
-        plist = plistlib.load(f)
-    band_size = plist['band-size']
-    logger.info('Band size: %d bytes', band_size)
+        self.package_queue = package_queue
+        self.stop_event = stop_event
 
-    # Gather the list of bands
-    bands_dir = os.path.join(bundle, 'bands')
-    if not os.path.exists(bands_dir):
-        logger.critical('Bundle bands directory does not exist: %s', bands_dir)
-        exit(1)
+        self.logger = logging.getLogger('packer')
 
-    bands_path = Path(bands_dir)
-    bands = []
-    for f in bundle_files:
-        if Path(f) == bands_path:
-            continue
+    def pack(self):
+        # Read band size
+        plist_path = os.path.join(self.bundle, 'Info.plist')
+        if not os.path.exists(plist_path):
+            raise RuntimeError(
+                'Info.plist does not exist: {}'.format(plist_path))
 
-        if bands_path not in Path(f).parents:
-            continue
+        with open(plist_path, 'rb') as f:
+            plist = plistlib.load(f)
+        band_size = plist['band-size']
+        self.logger.info('Band size: %d bytes', band_size)
 
-        base = os.path.basename(f)
-        try:
-            num = int(base, 16)
+        # Gather the list of bands
+        bands_dir = os.path.join(self.bundle, 'bands')
+        if not os.path.exists(bands_dir):
+            raise RuntimeError(
+                'Bundle bands directory does not exist: {}'.format(bands_dir))
 
-            if base != format(num, 'x'):
-                logger.critical('Invalid band file: %s', f)
-                exit(1)
-        except ValueError:
-            logger.critical('Invalid band file: %s', f)
-            exit(1)
-        bands.append(num)
-    bands = sorted(bands)
-    logger.info('Band count: %d', len(bands))
+        bands_path = Path(bands_dir)
+        bands = []
+        for f in self.bundle_files:
+            if Path(f) == bands_path:
+                continue
 
-    # Calculate packages
-    packages = {}
-    for band in bands:
-        package_id = band // package_count
-        if package_id not in packages:
-            packages[package_id] = []
-        packages[package_id].append(band)
-    logger.info('Package count: %d', len(packages))
+            if bands_path not in Path(f).parents:
+                continue
 
-    # Do packing
-    os.makedirs(outdir, exist_ok=True)
-    for package_id in sorted(packages.keys()):
-        while True:
-            outstanding_count = len(
-                glob.glob(os.path.join(outdir, '*.tar.gz')))
-            if outstanding_count < BACK_PRESSURE_LIMIT:
+            base = os.path.basename(f)
+            try:
+                num = int(base, 16)
+
+                if base != format(num, 'x'):
+                    raise RuntimeError('Invalid band file: {}'.format(f))
+            except ValueError:
+                raise RuntimeError('Invalid band file: {}'.format(f))
+            bands.append(num)
+        bands = sorted(bands)
+        self.logger.info('Band count: %d', len(bands))
+
+        # Calculate packages
+        packages = {}
+        for band in bands:
+            package_id = band // self.package_count
+            if package_id not in packages:
+                packages[package_id] = []
+            packages[package_id].append(band)
+        self.logger.info('Package count: %d', len(packages))
+
+        # Do packing
+        os.makedirs(self.outdir, exist_ok=True)
+        for package_id in sorted(packages.keys()):
+            while True:
+                outstanding_count = len(
+                    glob.glob(os.path.join(self.outdir, '*.tar.gz')))
+                if outstanding_count < BACK_PRESSURE_LIMIT:
+                    break
+                time.sleep(BACK_PRESSURE_SLEEP_INTERVAL)
+
+            start = format(package_id * self.package_count, 'x')
+            end = format((package_id + 1) * self.package_count - 1, 'x')
+            name = '{}-{}'.format(start, end)
+            tmp_path = os.path.join(self.outdir, '{}-tmp.tar.gz'.format(name))
+            path = os.path.join(self.outdir, '{}.tar.gz'.format(name))
+            done_path = os.path.join(self.outdir, '{}.done'.format(name))
+            self.logger.info('Packing package %s', path)
+
+            if os.path.exists(path) or os.path.exists(done_path):
+                self.logger.info('  Already done')
+                self.package_queue.put(name)
+                continue
+
+            if os.path.exists(tmp_path):
+                os.unlink(tmp_path)
+
+            with tarfile.open(tmp_path, 'x:gz') as tar:
+                bands = packages[package_id]
+                for band in bands:
+                    band_name = format(band, 'x')
+                    band_path = os.path.join(bands_dir, band_name)
+                    tar.add(band_path, band_name)
+            shutil.move(tmp_path, path)
+
+            self.package_queue.put(name)
+
+            if self.stop_event.is_set():
+                self.logger.info('Stopping...')
                 break
-            time.sleep(BACK_PRESSURE_SLEEP_INTERVAL)
 
-        start = format(package_id * package_count, 'x')
-        end = format((package_id + 1) * package_count - 1, 'x')
-        name = '{}-{}'.format(start, end)
-        tmp_path = os.path.join(outdir, '{}-tmp.tar.gz'.format(name))
-        path = os.path.join(outdir, '{}.tar.gz'.format(name))
-        done_path = os.path.join(outdir, '{}.done'.format(name))
-        logger.info('Packing package %s', path)
-
-        if os.path.exists(path) or os.path.exists(done_path):
-            logger.info('  Already done')
-            package_queue.put(name)
-            continue
-
-        if os.path.exists(tmp_path):
-            os.unlink(tmp_path)
-
-        with tarfile.open(tmp_path, 'x:gz') as tar:
-            bands = packages[package_id]
-            for band in bands:
-                band_name = format(band, 'x')
-                band_path = os.path.join(bands_dir, band_name)
-                tar.add(band_path, band_name)
-        shutil.move(tmp_path, path)
-
-        package_queue.put(name)
-
-        if stop_event.is_set():
-            logger.info('Stopping...')
-            break
-
-    package_queue.put(None)
+        self.package_queue.put(None)
